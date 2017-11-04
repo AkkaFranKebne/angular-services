eccf5d80212a2478e6dcde97182aaf496f92a436
AkkaFranKebne <ewawrobel@gmail.com>
2017-11-04 18:14:32 +0100

    adding console logging to service

--- src/app/active-users/active-users.component.ts
+++ src/app/active-users/active-users.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnChanges, OnInit, SimpleChanges} from '@angular/core';
 import {UsersService} from "../users.service";
 
 @Component({
@@ -8,6 +8,7 @@ import {UsersService} from "../users.service";
 })
 export class ActiveUsersComponent implements OnInit {
   users: string[];
+
 
   constructor(private usersService: UsersService) { }
 
--- src/app/users.service.ts
+++ src/app/users.service.ts
@@ -6,11 +6,15 @@ import {Injectable} from "@angular/core";
 export class UsersService {
   activeUsers =['Max', 'Anna'];
   inactiveUsers = ['Chris', "Manu"];
+  changeToActive = 0;
+  changeToInactive = 0;
 
   setToActive(id: number)
-    { console.log(this.inactiveUsers[id]);
+    {
       this.activeUsers.push(this.inactiveUsers[id]);
       this.inactiveUsers.splice(id,1);
+      this.changeToActive ++;
+      console.log( "changes to Active: " + this.changeToActive );
       //console.log(this.activeUsers);
       //console.log(this.inactiveUsers);
     };
@@ -19,6 +23,8 @@ export class UsersService {
   {
     this.inactiveUsers.push(this.activeUsers[id]);
     this.activeUsers.splice(id,1);
+    this.changeToInactive ++;
+    console.log( "changes to Inactive: " +  this.changeToInactive );
     //console.log(this.activeUsers);
     //console.log(this.inactiveUsers);
   };
